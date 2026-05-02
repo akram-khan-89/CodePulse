4160a7d218585b1277d8267229d571cf9d8b813c
Akram Khan <makramkhan8900@gmail.com>
2026-05-02 18:47:33 +0500

    feat: UI - Display Categories In BlogPost List Component

--- UI/CodePulse/src/app/features/blogpost/models/blogpost.model.ts
+++ UI/CodePulse/src/app/features/blogpost/models/blogpost.model.ts
@@ -1,3 +1,5 @@
+import { Category } from "../../category/models/category.model";
+
 export interface AddBlogPostRequest {
     title: string;
     shortDescription: string;
@@ -19,4 +21,5 @@ export interface BlogPost {
     author: string;
     publishedDate: Date;
     isVisible: boolean;
+    categories: Category[]
 }
