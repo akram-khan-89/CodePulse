71d5766ddb814fdefc198920d9c2220ef7c0b87a
Akram Khan <makramkhan8900@gmail.com>
2026-05-12 00:40:51 +0500

    feat: API - Upload Image Controller and Action Method

---
+++ API/CodePulse.API/CodePulse.API/Controllers/ImagesController.cs
@@ -0,0 +1,48 @@
+﻿using CodePulse.API.Models.Domain;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CodePulse.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ImagesController : ControllerBase
+    {
+
+        //POST: {apiBaseUrl}/api/images
+        [HttpPost]
+        public async Task<IActionResult> UploadImage([FromForm] IFormFile file,
+            [FromForm] string fileName, [FromForm] string title)
+        {
+            ValidateFile(file);
+
+            if (ModelState.IsValid)
+            {
+                //File upload
+                var blogImage = new BlogImage
+                {
+                    FileExtension = Path.GetExtension(file.FileName).ToLower(),
+                    FileName=fileName,
+                    Title=title,
+                    DateCreated=DateTime.Now,
+                };
+            }
+        }
+
+        private void ValidateFile(IFormFile file)
+        {
+            var allowedExtensions = new string[] { ".jpg", ".jpeg", ".png" };
+
+            if (!allowedExtensions.Contains(Path.GetExtension(file.FileName).ToLower()))
+            {
+                ModelState.AddModelError("file", "Unsupported file format");
+            }
+
+            //dont allow file size more than  10mb
+            if (file.Length > 10485768)
+            {
+                ModelState.AddModelError("file", "File Size Cannot be more than 10MB");
+            }
+        }
+    }
+}
